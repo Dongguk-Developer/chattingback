a89fc5507905ce5ab1de0bfbdb3afebad994d5ea
lh7721004 <lh7721004@gmail.com>
2024-10-06 21:41:50 +0900

    sendMessage add
    sendMessage add

--- db_util/connect.py
+++ db_util/connect.py
@@ -1,7 +1,9 @@
 import pymysql
+import os
 # DB에 접근하는 conn, cur 객체 생성 후 반환
 def mysql_create_session():
-    conn = pymysql.connect(host='api.studyhero.kr', user='studyhero-db', password='!Studyhero-db',port=3306, db='studyhero-db', 
+    conn = pymysql.connect(host=os.environ['DB_HOST'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'],port=os.environ['DB_PORT'], db=os.environ['DB_NAME'], 
                             charset='utf8', cursorclass=pymysql.cursors.DictCursor)
     cur = conn.cursor()
     return conn, cur
+    # 2024200200 여기 보안 부분 고쳐야함(os.environ쓰고 아직 커밋 안함)
--- main.py
+++ main.py
@@ -84,7 +84,15 @@ async def websocket_endpoint(websocket: WebSocket):
         data = await websocket.receive_text()  # client 메시지 수신대기
         print(f"message received : {data} from : {websocket.client}")
         await websocket.send_text(f"Message text was: {data}") # client에 메시지 전달
+@app.websocket("/sendMessage")
+async def websocket_endpoint(websocket: WebSocket):
+    print(f"client connected : {websocket.client}")
+    await websocket.accept() # client의 websocket접속 허용
+    await websocket.send_text(f"Welcome client : {websocket.client}")
+    while True:
+        data = await websocket.receive_text()  # client 메시지 수신대기
+        print(f"message received : {data} from : {websocket.client}")
+        await websocket.send_text(f"Message text was: {data}") # client에 메시지 전달
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8090,reload=True)
-
+    uvicorn.run("main:app", host="0.0.0.0", port=os.environ['BACKEND_PORT'],reload=True)
