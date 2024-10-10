7facaf7b539d6d163738102d839becd940ad7736
lce0517 <ashaeun2010@gmail.com>
2024-10-10 17:32:57 +0900

    [feat] kakao oauth 구현 / KAKAO_REDIRECT_URI : 카카오 API 세팅 페이지에서 Redirect URI부분 백엔드 포트 지정해야하는지 의문이라 서버 환경에서 backend api 테스트 후 수정해야 할 듯 함

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import pymysql
 import logging
-from db_util.queryCheck import query_check
-from db_util.connect import mysql_create_session
+# from db_util.queryCheck import query_check
+# from db_util.connect import mysql_create_session
 import socketio
+import httpx
+from fastapi.responses import RedirectResponse
+import requests
+
 origins = [
     "https://api.studyhero.kr",
     "https://studyhero.kr",
@@ -74,10 +78,64 @@ def update_calender():
 @app.post("/delete_calender")
 def delete_calender():
     pass
+
+# 카카오 OAuth
+KAKAO_CLIENT_ID = "458b13d8b3f70d003a62c112e6af767b"
+KAKAO_CLIENT_SECRET = "HMjhLHfjHsinsiWHBgA7RNgxjAJJQCOn"
+KAKAO_REDIRECT_URI = "http://localhost:8090/oauth/kakao/callback"
+KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize"
+KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token"
+KAKAO_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me"
+
+@app.get("/auth/kakao")
+async def auth_kakao():
+    print(f"{KAKAO_AUTH_URL}?client_id={KAKAO_CLIENT_ID}&redirect_uri={KAKAO_REDIRECT_URI}&response_type=code")
+    return RedirectResponse(
+        url=f"{KAKAO_AUTH_URL}?client_id={KAKAO_CLIENT_ID}&redirect_uri={KAKAO_REDIRECT_URI}&response_type=code"
+    )
+
+@app.get("/auth/kakao/callback")
+async def auth_kakao_callback(code: str):
+    async with httpx.AsyncClient() as client:
+        token_response = await client.post(
+            KAKAO_TOKEN_URL,
+            data={
+                "grant_type": "authorization_code",
+                "client_id": KAKAO_CLIENT_ID,
+                "redirect_uri": KAKAO_REDIRECT_URI,
+                "code": code,
+                "client_secret": KAKAO_CLIENT_SECRET,
+            },
+        )
+        token_response_data = token_response.json()
+        access_token = token_response_data.get("access_token")
+
+    if access_token:
+        async with httpx.AsyncClient() as client:
+            user_response = await client.get(
+                KAKAO_USER_INFO_URL,
+                headers={"Authorization": f"Bearer {access_token}"},
+            )
+            user_data = user_response.json()
+            return user_data
+
+    return {"error": "인증 실패"}
+
+@app.get("/oauth/kakao/callback")
+def oauth_callback(code:str):
+    payload = {"grant_type": "authorization_code",
+               "client_id": KAKAO_CLIENT_ID,
+               "redirect_uri": KAKAO_REDIRECT_URI,
+               "code": code,
+               "client_secret": KAKAO_CLIENT_SECRET}
+    headers = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8"}
+    req = requests.post("https://kauth.kakao.com/oauth/token",data=payload,headers=headers)
+
 sio = socketio.AsyncServer(async_mode='asgi') #socketio 서버 생성
 app = socketio.ASGIApp(sio, app) #메인 서버와 socketio서버 통합
 @sio.on('message') #message 이벤트에 대한 처리
 def another_event(sid, data):
     print(sid,data)
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ['BACKEND_PORT']),reload=True)
