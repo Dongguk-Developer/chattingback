3ad21b15fa9eb4282b8db4a9d91675db31060474
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-10-26 07:34:17 +0900

    python: 코드 정리

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 import uvicorn
-from fastapi import FastAPI,Request,WebSocket
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import pymysql
@@ -8,6 +8,7 @@ import logging
 from db_util.queryCheck import query_check
 from db_util.connect import mysql_create_session
 import socketio
+
 origins = [
     "https://api.studyhero.kr",
     "https://studyhero.kr",
@@ -26,58 +27,90 @@ app.add_middleware(
 
 
 @app.get("/")
-def read_root(request:Request):
+def read_root(request: Request):
     return {"Hello": "World"}
+
+
 @app.post("/get_profile")
 def get_profile():
     pass
+
+
 @app.post("/update_profile")
 def update_profile():
     pass
 
+
 @app.post("/get_ranking")
 def get_ranking():
     pass
+
+
 @app.post("/create_chatroom")
 def create_chatroom():
     pass
+
+
 @app.post("/update_chatroom")
 def update_chatroom():
     pass
+
+
 @app.post("/get_chatroom")
 def get_chatroom():
     pass
 
+
 @app.post("/create_emoji")
 def create_emoji():
     pass
+
+
 @app.post("/get_emoji")
 def get_emoji():
     pass
 
+
 @app.post("/create_studyplanner")
 def create_studyplanner():
     pass
+
+
 @app.post("/update_studyplanner")
 def create_studyplanner():
     pass
+
+
 @app.post("/delete_studyplanner")
 def delete_studyplanner():
     pass
 
+
 @app.post("/create_calender")
 def create_calender():
     pass
+
+
 @app.post("/update_calender")
 def update_calender():
     pass
+
+
 @app.post("/delete_calender")
 def delete_calender():
     pass
-sio = socketio.AsyncServer(async_mode='asgi') #socketio 서버 생성
-app = socketio.ASGIApp(sio, app) #메인 서버와 socketio서버 통합
-@sio.on('message') #message 이벤트에 대한 처리
+
+
+sio = socketio.AsyncServer(async_mode="asgi")  # socketio 서버 생성
+app = socketio.ASGIApp(sio, app)  # 메인 서버와 socketio서버 통합
+
+
+@sio.on("message")  # message 이벤트에 대한 처리
 def another_event(sid, data):
-    print(sid,data)
+    print(sid, data)
+
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ['BACKEND_PORT']),reload=True)
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.environ["BACKEND_PORT"]), reload=True
+    )
